9ede3b80f58729c12c5a95d3d0624622ed879a15
Filipa Vieira <91594330+FilipaChambel@users.noreply.github.com>
2023-10-10 12:40:10 +0100

    plot a line chart

--- JavaScript-Challenge-master/static/scripts.js
+++ JavaScript-Challenge-master/static/scripts.js
@@ -1,27 +1,27 @@
-const canvas = document.getElementById('chart')
-const ctx = canvas.getContext('2d')
+const canvas = document.getElementById("chart");
+const ctx = canvas.getContext("2d");
 
-function drawLine (start, end, style) {
-  ctx.beginPath()
-  ctx.strokeStyle = style || 'black'
-  ctx.moveTo(...start)
-  ctx.lineTo(...end)
-  ctx.stroke()
+function drawLine(start, end, style) {
+  ctx.beginPath();
+  ctx.strokeStyle = style || "black";
+  ctx.moveTo(...start);
+  ctx.lineTo(...end);
+  ctx.stroke();
 }
 
-function drawTriangle (apex1, apex2, apex3) {
-  ctx.beginPath()
-  ctx.moveTo(...apex1)
-  ctx.lineTo(...apex2)
-  ctx.lineTo(...apex3)
-  ctx.fill()
+function drawTriangle(apex1, apex2, apex3) {
+  ctx.beginPath();
+  ctx.moveTo(...apex1);
+  ctx.lineTo(...apex2);
+  ctx.lineTo(...apex3);
+  ctx.fill();
 }
 
-drawLine([50, 50], [50, 550])
-drawTriangle([35, 50], [65, 50], [50, 35])
+drawLine([50, 50], [50, 550]);
+drawTriangle([35, 50], [65, 50], [50, 35]);
 
-drawLine([50, 550], [950, 550])
-drawTriangle([950, 535], [950, 565], [965, 550])
+drawLine([50, 550], [950, 550]);
+drawTriangle([950, 535], [950, 565], [965, 550]);
 
 const spinner = document.getElementById("spinner");
 
@@ -35,7 +35,7 @@ fetch("http://localhost:3000/stocks")
     const stockSymbols = data.stockSymbols;
     console.log("List of available stocks: ", stockSymbols);
 
-      /**
+    /**
      * Hide the spinner after all data is loaded
      */
     spinner.style.display = "none";
@@ -47,91 +47,82 @@ fetch("http://localhost:3000/stocks")
     let allStocksData = []; //array that stores all stocks data
     let tableData = {};
     Promise.all(
-      
       stockSymbols.map(async (stockSymbol) => {
-       try {
-          const res = await fetch(`http://localhost:3000/stocks/${stockSymbol}`)
-          const stockData = await res.json()
+        try {
+          const res = await fetch(
+            `http://localhost:3000/stocks/${stockSymbol}`
+          );
+          const stockData = await res.json();
           // console.log(`Data for ${stockSymbol}: `);
           for (let i = 0; i < stockData.length; i++) {
             const formattedStockData = {
               Symbol: stockSymbol,
               Value: stockData[i].value,
               Timestamp: stockData[i].timestamp,
-            }
-//console.log(stockData)
-//console.log(formattedStockData);
+            };
+            //console.log(stockData)
+            //console.log(formattedStockData);
 
-            allStocksData.push(formattedStockData)
+            allStocksData.push(formattedStockData);
           }
-          console.log(allStocksData);
-          plotData(stockData)
+          // console.log(allStocksData);
+          plotData(stockData);
         } catch (error) {
-          console.error("Error retriving stock data", error)
-          return null
+          console.error("Error retriving stock data", error);
+          return null;
         }
       })
-      
     ).then(() => {
-        /**
-             * Log the data in a table
-             */
-        tableData = allStocksData.map((data) => {
-          return {
-            Symbol: data.Symbol,
-            Value: data.Value,
-            Timestamp: new Date(data.Timestamp).toLocaleString(), // Format the timestamp as date and time
-          };
-        });
+      /**
+       * Log the data in a table
+       */
+      tableData = allStocksData.map((data) => {
+        return {
+          Symbol: data.Symbol,
+          Value: data.Value,
+          Timestamp: new Date(data.Timestamp).toLocaleString(), // Format the timestamp as date and time
+        };
+      });
 
-        console.table(tableData);
- 
-      
+      console.table(tableData);
     });
-   
-    
   })
   .catch((error) => {
     console.error("Error retriving list of available stocks", error);
   });
- 
-  function plotData(stockData){
-const xAxis = canvas.width / (stockData.length-1);
-const yAxis = canvas.height / 100; //Consider stock values between 0 to 100
 
-ctx.beginPath();
+function plotData(stockData) {
+  const xAxis = canvas.width / (stockData.length - 1);
+  const yAxis = canvas.height / 100; //Consider stock values between 0 to 100
 
-//x-axis labels
-ctx.font ='8px';
-ctx.fillStyle = "black";
+  ctx.beginPath();
 
-for(let i =100; i<=1000; i+=100){
-  const xlabel = i * xAxis/100;
-  ctx.fillText(i.toString(), xlabel, canvas.height-30);//adjust labels to the height of the axis
-}
+  //x-axis labels
+  ctx.font = "8px";
+  ctx.fillStyle = "black";
 
-//y-axis labels
-for(let i=10; i<=1000; i+=10){
-  const ylabel = canvas.height - i * yAxis;
-  ctx.fillText(i.toString(), 10, ylabel)
-}
-
-
-for(let i = 0; i< stockData.length; i++){
-const x = i * xAxis;
-const y = canvas.height - stockData[i].value * yAxis;
-
-if(i===0){
-  ctx.moveTo(x, y)
-} else {
- 
-  ctx.lineWidth = 2;
-  ctx.lineTo(x,y);
-  ctx.stroke();
-
- 
-}
-
-}
-
+  for (let i = 100; i <= 1000; i += 100) {
+    const xlabel = (i * xAxis) / 100;
+    ctx.fillText(i.toString(), xlabel, canvas.height - 30); //adjust labels to the height of the axis
   }
+
+  //y-axis labels
+  for (let i = 10; i <= 100; i += 10) {
+    const ylabel = canvas.height - i * yAxis;
+    ctx.fillText(i.toString(), 10, ylabel);
+  }
+
+  for (let i = 0; i < stockData.length; i++) {
+    const x = i * xAxis + 50;//start plotting the graph after the drawLine
+    const y = canvas.height - stockData[i].value * yAxis;
+
+    if (i === 0) {
+      ctx.moveTo(x, y);
+    } else {
+      ctx.lineWidth = 2;
+      ctx.lineTo(x, y);
+      
+    }
+    ctx.stroke();
+  }
+}
