ca07b453faf52b9db724fc9478e6eb6e09a8a110
Filipa Vieira <91594330+FilipaChambel@users.noreply.github.com>
2023-10-10 08:47:08 +0100

    All main tasks completed
    Query the backend for list of available stocks
    Query the backend for data about each stock
    Hide the spinner after all data is loaded
    Log the data in the console
    Return a meaningful error when data cannot be retrieved

--- JavaScript-Challenge-master/app.js
+++ JavaScript-Challenge-master/app.js
@@ -5,15 +5,36 @@ const stocks = require('./stocks')
 const app = express()
 app.use(express.static(path.join(__dirname, 'static')))
 
-app.get('/stocks', async (req, res) => {
-  const stockSymbols = await stocks.getStocks()
-  res.send({ stockSymbols })
-})
+app.get("/stocks", async (req, res) => {
+  /**
+   * Return a meaningful error message
+   * when stock data cannot be retrieved
+   */
+  try {
+    const stockSymbols = await stocks.getStocks();
+    res.send({ stockSymbols });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to load stock data" });
+  }
+});
 
-app.get('/stocks/:symbol', async (req, res) => {
-  const { params: { symbol } } = req
-  const data = await stocks.getStockPoints(symbol, new Date())
-  res.send(data)
-})
+app.get("/stocks/:symbol", async (req, res) => {
+  const {
+    params: { symbol },
+  } = req;
+
+  /**
+   * Return a meaningful error message
+   * when stock data cannot be retrieved
+   */
+  try {
+    const data = await stocks.getStockPoints(symbol, new Date());
+    res.send(data);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Failed to retrieve stock data for stock item" });
+  }
+});
 
 app.listen(3000, () => console.log('Server is running!'))
--- JavaScript-Challenge-master/static/scripts.js
+++ JavaScript-Challenge-master/static/scripts.js
@@ -22,3 +22,106 @@ drawTriangle([35, 50], [65, 50], [50, 35])
 
 drawLine([50, 550], [950, 550])
 drawTriangle([950, 535], [950, 565], [965, 550])
+
+const spinner = document.getElementById("spinner");
+
+/**
+ * Query the backend for list of available stocks
+ */
+
+fetch("http://localhost:3000/stocks")
+  .then((res) => res.json())
+  .then((data) => {
+    const stockSymbols = data.stockSymbols;
+    console.log("List of available stocks: ", stockSymbols);
+
+      /**
+     * Hide the spinner after all data is loaded
+     */
+    spinner.style.display = "none";
+
+    /**
+     * Query the backend for data about each stock
+     */
+
+    Promise.all(
+      stockSymbols.map((stockSymbol) => {
+        fetch(`http://localhost:3000/stocks/${stockSymbol}`)
+          .then((res) => res.json())
+          .then((stockData) => {
+            console.log(`Data for ${stockSymbol}: `);
+            
+            for(let i = 0; i< stockData.length; i++){
+              const formattedStockData =  {
+                Symbol: stockSymbol,
+                Value: stockData[i].value,
+                Timestamp: stockData[i].timestamp,
+              };
+              
+              //log the data in an array format
+            console.log(stockData);
+            /**
+             * Log the data in a more structured way in a table format
+             */
+            console.table([formattedStockData]);
+
+            plotData(stockData);
+             // return formattedStockData;
+            
+            }
+           
+            // return formattedStockData;
+          })
+          .catch((error) => {
+            console.error("Error retriving stock data", error);
+            return null;
+          });
+      })
+    ).then((stockDataArray) => {
+      //console.table(stockDataArray.filter((data) => data !== null));
+    });
+  })
+  .catch((error) => {
+    console.error("Error retriving list of available stocks", error);
+  });
+
+  function plotData(stockData){
+const xAxis = canvas.width / (stockData.length-1);
+const yAxis = canvas.height / 100; //Consider stock values between 0 to 100
+
+ctx.beginPath();
+
+//x-axis labels
+ctx.font ='8px';
+ctx.fillStyle = "black";
+
+for(let i =100; i<=1000; i+=100){
+  const xlabel = i * xAxis/100;
+  ctx.fillText(i.toString(), xlabel, canvas.height-30);//adjust labels to the height of the axis
+}
+
+//y-axis labels
+for(let i=10; i<=1000; i+=10){
+  const ylabel = canvas.height - i * yAxis;
+  ctx.fillText(i.toString(), 10, ylabel)
+}
+
+
+for(let i = 0; i< stockData.length; i++){
+const x = i * xAxis;
+const y = canvas.height - stockData[i].value * yAxis;
+
+if(i===0){
+  ctx.moveTo(x, y)
+} else {
+ 
+  ctx.lineWidth = 2;
+  ctx.lineTo(x,y);
+  ctx.stroke();
+
+ 
+}
+
+}
+
+  }
